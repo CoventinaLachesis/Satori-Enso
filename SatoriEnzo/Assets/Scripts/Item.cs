0bbbf75e147a89d088bd579ddcac2d117bd810bd
EgapNimda <6434449023@student.chula.ac.th>
2025-02-23 20:10:07 +0700

    add 3 items

---
+++ SatoriEnzo/Assets/Scripts/Item.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public float bonusJumpSpeed = 0;
+    public float bonusHorizontalSpeed = 0;
+    public int bonusJump = 0;
+    public bool bonusShield = false;
+    public float duration;
+    
+}
